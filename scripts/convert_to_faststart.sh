b60e9ae95c05938aa92c5e15beabc70f7a3676ea
srad <ssedighi@posteo.de>
2022-03-02 03:32:08 +0100

    Fixes bash script

--- scripts/convert_to_faststart.sh
+++ scripts/convert_to_faststart.sh
@@ -9,6 +9,7 @@ dir_counter=1
 folders=$(ls -d $1/*)
 dir_total=$(ls -d $1/* | wc -l)
 
+# skip first
 for d in $folders; do
   echo
   echo "($dir_counter/$dir_total) Converting in dir: '$d'"
@@ -21,11 +22,17 @@ for d in $folders; do
   # Generate previews for all MP4 files.
   for i in *.mp4; do
       echo "($file_counter/$files_total) $i -> ${i%.*}.fast.mp4 -> ${i%.*}.mp4"
-      if ffmpeg -i "$i" -c copy -map 0 -movflags faststart -hide_banner -y -loglevel error -progress ${i%.*}.fast.mp4; then
-        echo rm "${i%.*}.mp4"
-        echo mv "${i%.*}.fast.mp4" "${i%.*}.mp4"
-        echo
+      if /home/saman/src/StreamSink.Go/scripts/mp4_to_faststart.sh .; then
         echo "completed $i"
+        cd .previews/videos
+        if /home/saman/src/StreamSink.Go/scripts/mp4_to_faststart.sh .; then
+          echo "ok previews $i"
+        else
+          echo "error preview $i"
+        fi
+        cd ..
+        cd ..
+        cd ..
       else
         echo "error:$i"
       fi
