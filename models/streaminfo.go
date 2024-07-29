0fb4caf2b82de24d6c24e5f9c4040f5f3e169a6b
srad <ssedighi@posteo.de>
2024-07-29 11:41:56 +0200

    Tabs to 4 spaces.

--- models/streaminfo.go
+++ models/streaminfo.go
@@ -1,22 +1,22 @@
 package models
 
 import (
-	"github.com/srad/streamsink/conf"
-	"github.com/srad/streamsink/helpers"
-	"path/filepath"
+    "github.com/srad/streamsink/conf"
+    "github.com/srad/streamsink/helpers"
+    "path/filepath"
 )
 
 type StreamInfo struct {
-	IsOnline      bool        `json:"isOnline" extensions:"!x-nullable"`
-	IsTerminating bool        `extensions:"!x-nullable"`
-	Url           string      `extensions:"!x-nullable"`
-	ChannelName   ChannelName `json:"channelName" extensions:"!x-nullable"`
+    IsOnline      bool        `json:"isOnline" extensions:"!x-nullable"`
+    IsTerminating bool        `extensions:"!x-nullable"`
+    Url           string      `extensions:"!x-nullable"`
+    ChannelName   ChannelName `json:"channelName" extensions:"!x-nullable"`
 }
 
 func (si *StreamInfo) Screenshot() error {
-	return helpers.ExtractFirstFrame(si.Url, conf.FrameWidth, filepath.Join(si.ChannelName.AbsoluteChannelDataPath(), SnapshotFilename))
+    return helpers.ExtractFirstFrame(si.Url, conf.FrameWidth, filepath.Join(si.ChannelName.AbsoluteChannelDataPath(), SnapshotFilename))
 }
 
 func GetStreamInfo() map[ChannelName]StreamInfo {
-	return streamInfo
+    return streamInfo
 }
--- services/event_dispatcher.go
+++ services/event_dispatcher.go
@@ -1,41 +1,41 @@
 package services
 
 import (
-	"context"
+    "context"
 
-	log "github.com/sirupsen/logrus"
-	"github.com/srad/streamsink/models"
-	"github.com/srad/streamsink/network"
+    log "github.com/sirupsen/logrus"
+    "github.com/srad/streamsink/models"
+    "github.com/srad/streamsink/network"
 )
 
 var (
-	dispatchCtx, dispatchCancel = context.WithCancel(context.Background())
+    dispatchCtx, dispatchCancel = context.WithCancel(context.Background())
 )
 
 // StartDispatch Forward all relevant messages to the websocket.
 func StartDispatch() {
-	go dispatchMessages(dispatchCtx)
+    go dispatchMessages(dispatchCtx)
 }
 
 func StopDispatch() {
-	dispatchCancel()
+    dispatchCancel()
 }
 
 func dispatchMessages(ctx context.Context) {
-	go dispatchJobInfo(ctx)
-	go DispatchRecorder(ctx)
-	go models.DispatchJob(ctx)
+    go dispatchJobInfo(ctx)
+    go DispatchRecorder(ctx)
+    go models.DispatchJob(ctx)
 }
 
 func dispatchJobInfo(ctx context.Context) {
-	for {
-		select {
-		case m := <-models.JobInfoChannel:
-			network.SendSocket(m.Name, m.Message)
-			return
-		case <-ctx.Done():
-			log.Infoln("[dispatchMessages] stopped")
-			return
-		}
-	}
+    for {
+        select {
+        case m := <-models.JobInfoChannel:
+            network.SendSocket(m.Name, m.Message)
+            return
+        case <-ctx.Done():
+            log.Infoln("[dispatchMessages] stopped")
+            return
+        }
+    }
 }
